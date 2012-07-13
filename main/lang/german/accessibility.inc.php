fac86d8b5de9a099f2fdbbdd4ca9781a25930457
Laurent Opprecht <Laurent Opprecht@localhost>
2012-07-13 14:19:52 +0200

    see #5188 migrate: Undefined index: activate_legal

---
+++ main/lang/german/accessibility.inc.php
@@ -0,0 +1,13 @@
+<?php
+$langClarContent = "<p> <b> Kurs-Leiter </ b> </ p> Chamilo ist ein Managementsystem zur Verbesserungt von Ausbildungs-und Kenntnisständen. Es ermöglicht Lehrern, Ausbildern, Ausbildungsleitern Online-Kurse in Lernarragemnts zu organisieren, Interaktionen mit den Lernenden zu verwalten und online Inhalte mit dem Browser zu erstellen. Seit Chamilo 1,8, ist es auch möglich, PowerPoint-Präsentationen zu Lernpfaden zu konvertieren und  Videokonferenzen (virtuelles Treffen und virtuelles Klassenzimmer) zu organisieren</ p> Um Chamilo als Kurs-Leiterr nutzen zu können, müssen Sie einen Benutzernamen und ein Passwort haben. Diese können entweder durch die Selbstregistrierung erhlaten  (wenn Ihr Portal dies ermöglicht, der Link \"Registrierung\" erscheint auf der Homepage) oder durch den Administrator, wenn die Anmeldung zentral verwaltet wird. Sobald Sie Ihren Benutzernamen und Ihr Passwort haben, melden Sie sich am System an, erstellen Sie einen Kurs (oder nutzen Sie den, der für Sie vom Adminitrator angelegt wurde  und machen Sie sich mit den Werkzeugen vertraut.</ p> ";
+$test = "Test";
+$WCAGImage = "Bild";
+$WCAGLabel = "Bildbezeichnung";
+$WCAGLink = "Link";
+$WCAGLinkLabel = "Linkbeschreibung";
+$errorNoLabel = "";
+$AllLanguages = "alle Sprachen";
+$WCAGEditor = "WCAG Editor";
+$WCAGGoMenu = "Gehe zu Menü";
+$WCAGGoContent = "Gehe zu Inhalt";
+?>
