8e157ae2deda38e6d4c213cad00f389b116c6434
Julio Montoya <gugli100@gmail.com>
2012-07-30 13:27:19 +0200

    Minor - adding static in some functions

---
+++ main/lang/catalan/help.inc.php
@@ -0,0 +1,95 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$langHFor = "Ajuda: Fòrums";
+$langForContent = " Un fòrum és una eina de debat asíncrona. Mentre que un correu electrònic permet un diàleg privat entre dues persones, en els fòrums aquest diàleg serà públic o semipúblic i podran intervenir més persones.Des del punt de vista tècnic, els usuaris només necessiten un navegador d'Internet per a usar els fòrums de la Plataforma.Per a organitzar els fòrums de debat, premi en 'Administració dels fòrums'. Els debats s'organitzen en categories i subcategories tal com segueix:Categoria > Fòrum > Temi > RespostesPer a estructurar els debats dels seus usuaris, és necessari organitzar per endavant les categories i els fòrums, deixant que ells siguin els quals creuen els temes i les possibles respostes. Per defecte, els fòrums de cada curs contenen dues categories: una reservada als grups del curs 'Fòrums de grups' i altra comuna al curs, denominada per defecte'Principal' (encara que aquest nom pot canviar-se); dintre d'aquesta última hi ha creat un 'Fòrum de proves' amb un tema d'exemple.El primer que ha de fer és esborrar el tema d'exemple i canviar el nom del fòrum de proves. Després pot crear, en la categoria públic, altres fòrums, bé sigui per grups o temes, per a ajustar-se als requisits de la seva proposta d'aprenentatge.No confongui les categories amb els fòrums, ni aquests amb els temes, i no oblidi que una categoria buida (sense fòrums) no apareixerà en la vista de l'usuari.Finalment, cada fòrum pot tenir una descripció que pot consistir en la llista dels seus membres, els seus objectius, temàtica...Els fòrums dels grups no han de crear-se a través de l'eina 'Fòrums', sinó mitjançant l'eina 'Grups'; en aquesta última podrà decidir si els fòrums del grup seran privats o públics.Ús pedagògic avançatAlguns professors utilitzen el fòrum per a realitzar correccions. Un estudiant publica un document. El professor ho corregeix usant el botó marcador de l'editor HTML (marca amb un color la correcció o els errors), de manera que altres estudiants i professors podran beneficiar-se d'elles.";
+$langHDropbox = "Ajuda: Compartició de fitxers";
+$langDropboxContent = "Compartir documents és una eina de gestió de continguts dirigida a l'intercanvi de dades entre iguals (p2p). Qualsevol tipus de fitxer és acceptat: Word, Excel, PDF, etc. Generarà diferents versions en els enviaments, amb el que evitarà la destrucció d'un document quan s'enviï altre amb el mateix nom..Els documents compartits mostren els arxius que li han enviat (carpeta d'arxius rebuts) i els arxius que Vè. ha enviat a altres membres d'aquest curs (carpeta d'arxius enviats)Si la llista d'arxius rebuts o enviats es fa massa llarga, pot suprimir tots o alguns arxius de la mateixa. L'arxiu sí mateix no s'elimina mentre l'altre usuari ho mantingui en la seva.Per a enviar un document a més d'una persona, ha d'utilitzar CTRL+clic per a seleccionar-los en la caixa de selecció múltiple. La caixa de selecció múltiple és el formulari que mostra la llista de membres.";
+$langHHome = "Ajuda: Pàgina inicial";
+$langHomeContent = "<p>La pàgina inicial del vostre curs presenta una sèrie d'eines: un text d'introducció, una descripció del curs, una eina de publicació de Documents, etc. Aquesta pàgina és modular. Podeu visualitzar o ocultar cadascún dels elements clicant + o - (a vegades l'icona Ull tancat, Ull obert).</p><b>Navegació</b><p>Per moure's pel curs, disposa de dues eines de navegació: una a la part superior esqueera, que mostra el lloc en el qual es troba el curs, l'altra en la part superior dreta que li permet accedir a qualsevol eina mitjançant un clic en la seva icona. Si selecciona 'Curs actual' a l'esquerra o l'icona en forma de casa situat a la dreta, anirà a la pàgina principal del curs.</p><b>Metodologia</b><p>És important que dinamitzeu el vostre espai a fi que els alumnes percebin que hi ha algú darrera la pantalla. D'aquesta manera podeu modificar regularment el text d'introducció (clicant sobre el llapis groc) per indicar els esdeveniments del curs o recordar-ne les etapes.</p><p>Per construir el vostre curs, una manera clàssica de treballar és procedir com segueix: <ol><li>A propietats de curs, marqueu Accés: privat i Inscripció: refusada per tal d'impedir qualsevol visita durant la creació del curs,</li><li>Mostreu totes les eines clicant sobre el vincle gris 'Mostrar' sota el nom de les eines marcades,</li><li>Utilitzeu les eines per omplir el vostre curs de continguts, esdeveniments, grups, etc.,</li><li>Desactiveu totes les eines</li><li>Utilitzeu l'eina Itinerari per construir un itinerari a través de les altres eines</li><li>Poseu l'Itinerari creat visible. Cliqueu sobre 'Vista alumne\" per veure el curs des del punt de vista d'aquell qui el segueix.</li></ol>";
+$langHOnline = "Ajuda: Sistema de conferència en directe";
+$langOnlineContent = "<b>Dokeos Live Conferencing</b> <p>Dokeos Live Conferencing (DLC) és una eina de videoconferència basada sobre el web i que utilitza la tecnologia Flash. Us permet organitzar facilament reunions virtuals entre 2, 3 o 4 llocs distants així com classes virtuals (interacció entre un formador i fins a 500 alumnes).</p> <p>Per organitzar una reunió virtual, comenceu per importar una presentació PowerPoint sota la pestanya Presentació (zona inferior esquerra), a continuació fixeu una trobada per la reunió utilitzant l'Agenda de Chamilo. En el moment de la reunió, haureu de disposar d'una webcam i uns auriculars amb micròfon. El so, en particular el del micròfon, haurà d'estar ben configurat. La configuració es pot realitzar a través de l'eina de paràmetres d'audio del vostre ordinador, així com per un clic dret sobre la zona dels videos a DLC i Paràmetres de Flash. </p><p>DLC requereix com a mínim Flash 9 i una connexió internet equivalent a l'ADSL. Tingueu en compte que es poden produir problemes d'interrupció a causa del FireWall de la vostra organització. Contacteu amb l'Administrador del Sistema.</p><p>En mode Reunió Virtual (quatre a quatre), cadascú pot esdevenir moderador, difondre la seva presentació PowerPoint, escriure o dibuixar i fer-se escoltar pels altres. En mode Classe Virtual (ú a molts), hi ha un mestre que dirigeix les operacions, difon la informació i és vist i sentit per tothom. Els altres segueixen passivament, pregunten pel xat o demanen la paraula. En mode Classe Virtual, només el formador té necessitat de webcam.</p>";
+$langHClar = "Ajuda: Chamilo";
+$langHDoc = "Ajuda: Documents";
+$langDocContent = "Els professors poden crear pàgines web simples ('Crear un document HTML') o transferir a aquesta secció, arxius de qualsevol tipus (HTML, *Word, *PowerPoint, *Excel, PDF, *Flash, *QuickTime, etc.). Tingui en compte que no tots els arxius que enviï podran ser vists pels altres usuaris, qui haurien de disposar de les eines apropiades per a obrir-los, en cas contrari, al fer clic sobre el nom de l'arxiu tan sols podran descarregar-lo. Aquesta descàrrega sempre serà possible si premen sobre la icona . No oblidi revisar prèviament amb un antivirus els fitxers que s'enviï al servidor.Els documents es presenten en la pantalla per ordre alfabètic. Si desitja que els documents s'ordenin de manera diferent, pot *renombrarlos fent que vagin precedits d'un nombre (01, 02, 03, ...). També pot usar l'eina lliçons per a presentar una sofisticada taula de continguts. Tingui en compte que quan transfereix documents al servidor, pot decidir no mostrar la secció 'Documents' i només mostrar una pàgina d'inici (afegir un enllaç a la pàgina web principal de l'activitat) i/o unes Lliçons que contingui només algun dels arxius de la secció Documents. Transferència de documents1. Situï's en la carpeta del mòdul ' Documents' on vol enviar els arxius (per defecte al directori arrel del curs).2. Premi sobre l'opció 'Enviar un document'; això li durà a una pantalla en la qual seleccionarà el document del seu ordinador amb l'ajuda del botó .3. Transfereixi el document a la web del curs prement el botó .4. Si el nom del document conté accents o altres caràcters especials pot ser que hagi de *renombrarlo perquè s'obri correctament. També és possible enviar diversos documents en un arxiu comprimit en format *zip i ordenar, si així ho desitja, que es descomprimeixi automàticament en el servidor. A més d'arxius *zip convencionals es poden enviar arxius *SCORM comprimits, que també tindran l'extensió *zip. Els continguts *SCORM són tutorials especials que han estat dissenyats d'acord amb una norma internacional: *SCORM. És un format especial perquè els continguts educatius puguin executar-se i intercanviar-se lliurement entre diferents Sistemes de Gestió del Coneixement (*LMS= *Learning *Management *Systems). En altres paraules, els materials *SCORM són independents de la plataforma, sent la seva importació i exportació molt simple. La gestió d'aquests arxius es realitza a través de l'eina Lliçons. Tingui en compte que l'administrador de la plataforma ha definit una grandària màxima per a qualsevol arxiu que transfereixi. Si desitja enviar arxius majors (per ex., vídeos...) posi's en contacte amb ell. Observacions especials per a l'enviament de pàgines web.L'enviament de pàgines web simples no planteja cap problema, encara que si la seva complexitat és major pot ser que no tinguin el funcionament esperat. En aquests casos es recomana empaquetar les seves pàgines web com arxius *SCORM comprimits i usar l'eina Lliçons (veure més amunt).Gestió de directoris i arxius Crear una carpeta.Això li permetrà organitzar el contingut de la secció 'Documents' guardant els documents en diferents carpetes o directoris. Pot crear tantes *subcarpetas com desitgi.1. Fer clic sobre 'Crear un directori', situat en la part superior2. Introdueixi el nom del nou directori.3. Validi fent clic en . Esborrar un directori o un arxiu.1. Faci clic en el botó de la columna 'Modificar'. Canviar el nom d'un directori o d'un arxiu.1. Faci clic en el botó de la columna 'Modificar'.2. Introdueixi el nou nom.3. Validi fent clic en . Moure un directori o un arxiu a altre directori.1. Faci clic sobre el botó de la columna 'Modificar'2. Esculli la carpeta a la qual vol moure el document, fent clic sobre el menú desplegable (la paraula \"arrel\" en aquest menú representa el directori principal de la secció 'Documents').3. Validi fent clic en . Afegir un comentari a un document o a una carpeta1. Faci clic en el botó de la columna 'Modificar'2. Introdueixi, modifiqui o esborri el comentari en la zona prevista.3. Validi fent clic en . Ocultar una carpeta o un document als membres de l'activitat.1. Faci clic en el botó de la columna 'Modificar' El document o el directori continua existint, però ja no serà visible per als membres de l'activitat. Si desitja que aquest element torni a ser visible, faci clic en el botó . Veure una carpeta o un arxiu.Per a veure el contingut d'una carpeta bastarà prémer sobre el seu nom. En el cas d'un arxiu, el procediment és similar, encara que hauríem de tenir instal·lats els programes necessaris per a la seva visualització, en cas contrari intentarà descarregar-los. S'ha de tenir especial cura amb els arxius d'extensions executables, els quals recomanem siguin escanejats amb un antivirus quan es descarreguin.Veure diverses imatges com una presentacióQuan el sistema detecta l'existència d'imatges en una carpeta, s'activa l'opció Mostrar presentació', al costat de l'Ajuda. Aquesta permet veure aquestes imatges de forma seqüencial. Com en qualsevol presentació, convé recordar que les imatges no només poden consistir en fotos, sinó també esquemes, mapes conceptuals, etc.Creació i edició de documents en format HTMLPot crear i editar directament en el servidor un document en format HTML sense sortir del seu navegador. Per a crear un document web, faci clic sobre ' Crear un document', doni-li un nom (eviti que el nom contingui accents o altres caràcters especials), i utilitzi l'editor per a compondre el document. Per a modificar el contingut d'un document web, faci clic en el botó de la columna 'Modificar', i es presentarà un editor web a més de les possibilitats de *renombrar i afegir un comentari al document.Sobre l'editor HTML de la Plataforma.L'editor de documents HTML és del tipus *WYSIWYG (*What *You *See *Is *What *You *Get=Ho que vés és el que obtindrà), el que permet compondre'ls sense haver d'emplenar línies de codi HTML, encara que podrà veure el codi prement sobre el botó ' Un menú amb diversos botons li facilitarà l'elecció del tipus i grandària de lletra, sagnar, fer llistes, acolorir, crear enllaços, taules, inserir imatges, etc. També és possible tallar i pegar. Es tracta d'un editor elemental, però que no precisa de cap altre programa addicional al seu navegador.Crear una LliçóAquesta utilitat li permet construir lliçons amb el contingut de les activitats. El resultat formarà una taula de matèries, però amb més possibilitats. Per a més informació, anar al módul";
+$langHUser = "Ajuda: Usuaris";
+$langHExercise = "Ajuda: Exercicis";
+$langHPath = "Ajuda: Itineraris formatius";
+$langHDescription = "Ajuda: Descripció del curs";
+$langHLinks = "Ajuda: Enllaços";
+$langHMycourses = "Ajuda: Àrea d'usuari";
+$langHAgenda = "Ajuda: Agenda";
+$langHAnnouncements = "Ajuda: Tauler d'anuncis";
+$langHChat = "Ajuda: Xat";
+$langHWork = "Ajuda: Treballs dels estudiants";
+$langHTracking = "Ajuda: Estadístiques";
+$langUserContent = "<b>Afegeix usuaris</b>
+
+L'opció 'Inscriu usuaris al curs' li permet afegir al seu curs usuaris ja registrats a la plataforma. Per a això comproveu primer si ja està registrat a la plataforma, en aquest cas, marqui la casella que apareix al costat del seu nom i validi, això ho inscriure en el curs. Si encara no està registrat a la plataforma, aquest registre haurà de realitzar l'administrador de la plataforma o el propi usuari en el cas del que aquesta opció estigui habilitada.
+
+Una segona possibilitat és que els estudiants s'inscriguin per si mateixos, per a això l'administrador del curs haurà d'haver activat a l'eina 'Configuració del curs'.
+
+Tant en les operacions de registre com en les d'inscripció els usuaris rebran un correu electrònic recordant-lis el seu nom d'usuari i contrasenya.
+
+
+<b>Descripció</b>
+
+La descripció no atorga ninguns privilegis en el sistema informàtic. Només indica als usuaris, qui és qui. Podeu modificar aquesta descripció, fent clic a la icona en forma de llapis i escrivint la funció que voleu descriure de cada usuari: professor, ajudant, estudiant, visitant, expert, documentalista, moderador, tutor ...
+
+<b>Drets d'administració</b>
+
+Per contra, els permisos o drets d'administració atorguen privilegis al sistema informàtic, podent modificar el contingut i l'organització del lloc del curs. Aquests privilegis presenten dos perfils. El perfil de 'Administrador del curs', en què la persona en qüestió tindrà els mateixos permisos que qui els hi està donant. El perfil de 'Tutor' que l'identificarà per fer-se càrrec dels grups que puguin establir en el curs. Per atorgar o no aquests permisos a un usuari del curs, n'hi haurà prou amb marcar o no la casella corresponent, després d'haver premut en l'opció modifica.
+
+<b>Cotitulars</b>
+
+Per fer que figuri el nom d'un cotitular del curs a la capçalera del mateix, aneu a la pàgina principal del curs i feu ús de l'eina 'Configuració del curs '. En aquesta eina modifiqueu el camp 'Professor'; aquest camp és completament independent de la llista d'usuaris del curs, de manera que pot no estar inscrit en el mateix.
+
+<b>Seguiment i àrees personals dels usuaris</b>
+
+A més d'oferir un llistat d'usuaris i modificar els seus permisos, l'eina 'Usuaris' dóna un seguiment individual i permet al professor definir capçaleres addicionals a la fitxa de cada estudiant, perquè aquests les emplenin. Aquestes dades addicionals només estaran vinculats al curs en qüestió.";
+$langGroupContent = "<b>Introducció</b>
+
+Aquesta eina permet crear i gestionar grups dins del seu curs. Quan es crea el curs (Crear grups), els grups estan buits. Hi ha moltes formes d'emplenar:
+
+<ul>
+<li> Automàticament ('Emplenar grups'),</li>
+<li> Manualment ('Editar'),</li>
+<li> Adscripció a un grup per part dels propis estudiants (Modificar característiques: 'Es permet als estudiants ...').</li>
+</ul>
+
+Es poden combinar aquestes tres formes. Pot, per exemple, demanar als estudiants que s'inscriguin en un grupo.Más tarda pot descobrir que algun no ho va fer i decideixi finalment omplir de forma automáticalos grups per completar-los. També podeu editar cada grup per decidir qui forma part de quin grup.
+Omplir grups, tant de forma manual o automàtica només és efectiu si hi ha estudiants inscritosen el curs (no confongui la inscripció en el curs amb la inscripció en els grups). La llista d'estudiants és visible en el mòdul Usuaris.
+
+<hr noshade=\"\" size=\"1\">
+
+<b>Crear Grups</b>
+
+Per crear grups nous, cliqueu a 'Crear nous grups' i determinar el nombre de grups que vol crear.El nombre màxim de membres és il·limitat, però és recomanable que indiqui un. Si deixa el camp nombre màxim sense emplenar, la mida serà infinit.
+
+<hr noshade=\"\" size=\"1\">
+
+<b>Característiques dels grups</b>
+
+Podeu determinar les característiques dels grups de forma global (per a tots els grups). Es permet als estudiants inscriure's en el grup que vulguin:
+
+Podeu crear grups buits, perquè els estudiants s'inscriguin. Si vostè ha definit un nombre màxim, els grups complets no acceptaran nous membres. Aquest mètode és bo per a professors que encara no coneixen la llista d'estudiants quan creen els grups.
+
+<hr noshade=\"\" size=\"1\">
+
+<b>Eines</b>
+
+Cada grup pot disposar d'un 'Fòrum' (privat o públic) i/o d'una àrea de 'Documents' (privada o pública)
+
+<hr noshade=\"\" size=\"1\">
+
+<b>Edició manual</b>
+
+Quan es creen els grups (Crear grups), veurà a la part inferior de la pàgina una llista dels grups amb un seguit d'informació i funcions:
+
+<ul>
+<li><b>Edita</b> manualment el nom del grup, descripció, tutor, llista de membres</li>
+<li><b>Esborra</b> elimina un grup</li>
+
+<hr noshade=\"\" size=\"1\">";
+?>
