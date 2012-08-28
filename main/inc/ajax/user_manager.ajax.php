5268583c2385e039f9cb84140c1175e9bb6165cb
Hubert Borderiou <baelmyhu@gmail.com>
2012-08-28 16:00:13 +0200

    Merge in 1.9.x

--- main/inc/ajax/user_manager.ajax.php
+++ main/inc/ajax/user_manager.ajax.php
@@ -19,9 +19,13 @@ switch ($action) {
             echo '<img src="'.$user_info['avatar'].'" /> ';
             echo '</div>';
             echo '</div>';
-            echo '<div class="span2">';
-            
-            echo '<h3>'.$user_info['complete_name'].'</h3> '.$user_info['mail'].' '.$user_info['official_code'];
+            echo '<div class="span3">';            
+            if (api_get_setting('show_email_addresses') == 'false') {
+                $user_info['mail'] = ' ';
+            } else {
+                $user_info['mail'] = ' '.$user_info['mail'].' ';
+            }
+            echo '<h3>'.$user_info['complete_name'].'</h3>'.$user_info['mail'].$user_info['official_code'];
             echo '<br/><br/><a class="btn" href="'.api_get_path(WEB_CODE_PATH).'social/profile.php?u='.$user_info['user_id'].'">'.get_lang('ViewSharedProfile').'</a>';
             echo '</div>';
             echo '</div>';
@@ -42,9 +46,9 @@ switch ($action) {
         if (api_is_anonymous()) {
 			echo '';	
 		} else {		
-			$field_id = intval($_GET['field_id']);
-			$tag = $_GET['tag'];
-			echo UserManager::get_tags($tag, $field_id,'json','10');
+			if (isset($_GET['tag']) && isset($_GET['field_id'])) {
+                echo UserManager::get_tags($_GET['tag'], $_GET['field_id'],'json','10');
+            }
 		}
         break;
 	case 'generate_api_key':
