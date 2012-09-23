811cdf1fbbb6150aec6b518bd565874559456432
ywarnier <ywarnier@localhost>
2012-09-23 00:39:33 -0500

    Added basis of migration framework

---
+++ main/lang/greek/learnpath.inc.php
@@ -0,0 +1,42 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$lang_learning_path = "Μονοπάτι γνώσης";
+$lang_learning_path_builder = "Κατασκευαστής μονοπατιού γνώσης";
+$lang_description = "περιγραφή";
+$LearnpathTitle = "τίτλος";
+$LearnpathPrerequisites = "Προϋποθέσεις";
+$LearnpathMoveUp = "Μετακίνηση επάνω";
+$LearnpathMoveDown = "Μετακίνηση κάτω";
+$LearnpathTitleAndDesc = "Τίτλος και περιγραφή";
+$LearnpathAddPrereqi = "Προσθήκη προϋποθέσεων";
+$LearnpathAddTitleAndDesc = "Επεξεργασία τίτλου και περιγραφής";
+$lang_delete = "Διαγραφή";
+$langLearnpathFailed = "απέτυχε";
+$langLearnpathPrevious = "Προηγούμενο";
+$langLearnpathNext = "Επόμενο";
+$langLearnpathRestart = "Επανεκκίνηση";
+$langLearnpathLessonTitle = "Τίτλος βήματος";
+$langLearnpathStatus = "Κατάσταση";
+$langLearnpathScore = "Βαθμολογία";
+$langLearnpathTime = "Χρόνος";
+$langLearnpathVersion = "έκδοση";
+$langLearnpathRestarted = "Όλα τα βήματα είναι ημιτελή";
+$langLearnpathNoNext = "Αυτό είναι το τελευταίο βήμα";
+$LearnpathAddLearnpath = "Προσθήκη νέου μονοπατιού γνώσης";
+$LearnpathEditLearnpath = "επεξεργασία μονοπατιού γνώσης";
+$LearnpathDeleteLearnpath = "διαγραφή μονοπατιού γνώσης";
+$LearnpathPublish = "δημοσίευση";
+$LearnpathNotPublished = "δεν δημοσιεύτηκε";
+$LearnpathPublished = "δημοσιεύτηκε";
+$lang_add_prereq = "Προσθήκη /επεξεργασία πρϋποθέσεων σ' αυτό το βήμα";
+$lang_delete_learnpath_item = "Διαγραφή βήματος";
+$lang_author = "Συγγραφέας";
+$lang_date = "Ημερομηνία";
+$LearnpathPrerequisitesLimit = "Προϋποθέσεις (όριο)";
+$lang_loading = "Φορτώνει, παρακαλώ περιμένετε ...";
+$lang_empty = "Αυτό το μονοπάτι δεν περιέχει κανένα βήμα";
+$langLearnpathFullScreen = "πλήρης οθόνη";
+$langDone = "έγινε";
+?>
