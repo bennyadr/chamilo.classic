8502508080c536cb89172ab2babb1f14c1859628
ywarnier <ywarnier@localhost>
2012-09-22 19:48:58 -0500

    Fixed missing e-mail sending from contact form in Chamilo install

---
+++ main/lang/persian/slideshow.inc.php
@@ -0,0 +1,19 @@
+<?php
+/*
+for more information: see languages.txt in the lang folder.
+*/
+$Height = "ارتفاع";
+$ResizingComment = "تغییر اندازه عکس بر طبق اندازه های زیر بر حسب واحد پیکسل";
+$Width = "عرض";
+$Resizing = "تغییر اندازه پنجره";
+$NoResizingComment = "نمایش تمام عکس ها در اندازه اصلی آنها. هیچ تغییری در اندازه عکس ها ایجاد نمی شود. اگر اندازه عکس ها از اندازه صفحه نمایش بزرگتر باشد، میله های مخصوص تغییر مکان در صفحه به طور خودکار در گوشه های صفحه ایجاد خواهند شد";
+$ShowThumbnails = "نمایش علائم";
+$SetSlideshowOptions = "گزینش طرز نمایش";
+$SlideshowOptions = "گزینه های مربوظ به طرز نمایش";
+$NoResizing = "لطفاً اندازه پنجره را تغییر ندهید";
+$ExitSlideshow = "خروج";
+$SlideShow = "قسمت عکسها";
+$PreviousSlide = "عکس قبلی";
+$NextSlide = "عکس بعدی";
+$ViewSlideshow = "بازدید از قسمت عکس ها";
+?>
