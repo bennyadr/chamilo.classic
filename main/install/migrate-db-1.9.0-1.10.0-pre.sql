6afa50b8b4087a88c8077fc2fc3d73741128dd12
Julio Montoya <gugli100@gmail.com>
2012-10-25 13:35:57 +0200

    Fixing PHP warning

---
+++ main/install/migrate-db-1.9.0-1.10.0-pre.sql
@@ -0,0 +1,75 @@
+-- This script updates the databases structure before migrating the data from
+-- version 1.9.0 (or version 1.9.2, 1.9.4) to version 1.10.0
+-- it is intended as a standalone script, however, because of the multiple
+-- databases related difficulties, it should be parsed by a PHP script in
+-- order to connect to and update the right databases.
+-- There is one line per query, allowing the PHP function file() to read
+-- all lines separately into an array. The xxMAINxx-type markers are there
+-- to tell the PHP script which database we're talking about.
+-- By always using the keyword "TABLE" in the queries, we should be able
+-- to retrieve and modify the table name from the PHP script if needed, which
+-- will allow us to deal with the unique-database-type installations
+
+-- xxMAINxx
+
+-- Optimize tracking query very often queried on busy campuses
+ALTER TABLE track_e_online ADD INDEX idx_trackonline_uat (login_user_id, access_url_id, login_date);
+ALTER TABLE track_e_default ADD COLUMN session_id INT NOT NULL DEFAULT 0;
+
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('session_tutor_reports_visibility', NULL, 'radio', 'Session', 'true', 'SessionTutorsCanSeeExpiredSessionsResultsTitle', 'SessionTutorsCanSeeExpiredSessionsResultsComment', NULL, NULL, 1);
+INSERT INTO settings_options (variable, value, display_text) VALUES ('session_tutor_reports_visibility', 'true', 'Yes');
+INSERT INTO settings_options (variable, value, display_text) VALUES ('session_tutor_reports_visibility', 'false', 'No');
+
+INSERT INTO settings_current (variable, subkey, type, category, selected_value, title, comment, scope, subkeytext, access_url_changeable) VALUES ('gradebook_show_percentage_in_reports', NULL,'radio','Gradebook','true','GradebookShowPercentageInReportsTitle','GradebookShowPercentageInReportsComment', NULL, NULL, 0),
+INSERT INTO settings_options (variable, value, display_text) VALUES ('gradebook_show_percentage_in_reports', 'true', 'Yes');
+INSERT INTO settings_options (variable, value, display_text) VALUES ('gradebook_show_percentage_in_reports', 'false', 'No');
+
+ALTER TABLE notification ADD COLUMN sender_id INT NOT NULL DEFAULT 0;
+
+ALTER TABLE session_rel_user ADD COLUMN moved_to INT NOT NULL DEFAULT 0;
+ALTER TABLE session_rel_user ADD COLUMN moved_status INT NOT NULL DEFAULT 0;
+ALTER TABLE session_rel_user ADD COLUMN moved_at datetime NOT NULL default '0000-00-00 00:00:00';
+
+ALTER TABLE session ADD COLUMN display_start_date datetime NOT NULL default '0000-00-00 00:00:00';
+ALTER TABLE session ADD COLUMN display_end_date datetime NOT NULL default '0000-00-00 00:00:00';
+ALTER TABLE session ADD COLUMN access_start_date datetime NOT NULL default '0000-00-00 00:00:00';
+ALTER TABLE session ADD COLUMN access_end_date datetime NOT NULL default '0000-00-00 00:00:00';
+ALTER TABLE session ADD COLUMN coach_access_start_date datetime NOT NULL default '0000-00-00 00:00:00';
+ALTER TABLE session ADD COLUMN coach_access_end_date datetime NOT NULL default '0000-00-00 00:00:00';
+
+ALTER TABLE grade_components ADD COLUMN prefix VARCHAR(255) DEFAULT NULL;
+ALTER TABLE grade_components ADD COLUMN exclusions INT DEFAULT 0;
+ALTER TABLE grade_components ADD COLUMN count_elements INT DEFAULT 0;
+
+CREATE TABLE IF NOT EXISTS session_field_options (id int NOT NULL auto_increment, field_id int NOT NULL, option_value text, option_display_text varchar(255), option_order int, tms DATETIME NOT NULL default '0000-00-00 00:00:00', PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS course_field_options (id int NOT NULL auto_increment, field_id int NOT NULL, option_value text, option_display_text varchar(255), option_order int, tms DATETIME NOT NULL default '0000-00-00 00:00:00', PRIMARY KEY (id));
+
+ALTER TABLE session MODIFY COLUMN name CHAR(150) NOT NULL DEFAULT '';
+ALTER TABLE session MODIFY COLUMN id MEDIUMINT unsigned NOT NULL;
+
+ALTER TABLE session_rel_course MODIFY COLUMN id_session MEDIUMINT unsigned NOT NULL;
+ALTER TABLE session_rel_course ADD COLUMN course_id INT NOT NULL DEFAULT '0';
+ALTER TABLE session_rel_course ADD INDEX idx_session_rel_course_course_id (course_id);
+ALTER TABLE session_rel_course DROP PRIMARY KEY;
+ALTER TABLE session_rel_course ADD PRIMARY KEY (id_session, course_id);
+
+ALTER TABLE session_rel_course_rel_user MODIFY COLUMN id_session MEDIUMINT unsigned NOT NULL;
+ALTER TABLE session_rel_course_rel_user ADD COLUMN course_id INT NOT NULL DEFAULT '0';
+ALTER TABLE session_rel_course_rel_user DROP PRIMARY KEY;
+ALTER TABLE session_rel_course_rel_user ADD PRIMARY KEY (id_session, course_id, id_user);
+
+ALTER TABLE session_rel_course_rel_user ADD INDEX idx_session_rel_course_rel_user_id_user (id_user);
+ALTER TABLE session_rel_course_rel_user ADD INDEX idx_session_rel_course_rel_user_course_id (course_id);
+
+-- Courses changes c_XXX
+
+ALTER TABLE c_lp_item ADD INDEX idx_c_lp_item_cid_lp_id (c_id, lp_id);
+ALTER TABLE c_lp_item_view ADD INDEX idx_c_lp_item_view_cid_lp_view_id_lp_item_id(c_id, lp_view_id, lp_item_id);
+ALTER TABLE c_item_property ADD INDEX idx_itemprop_id_tool (c_id, tool(8));
+ALTER TABLE c_tool_intro MODIFY COLUMN intro_text MEDIUMTEXT NOT NULL;
+
+ALTER TABLE session ADD INDEX idx_id_coach (id_coach);
+ALTER TABLE session ADD INDEX idx_id_session_admin_id (session_admin_id);
+
+-- Do not move this 
+UPDATE settings_current SET selected_value = '1.10.0.20039' WHERE variable = 'chamilo_database_version';
---
+++ tests/migrate/migration.class.php
@@ -0,0 +1,357 @@
+<?php
+
+/**
+ * Scipt defining the Migration class
+ */
+
+/**
+ * Migration class (ease the migration work). This class *must* be extended
+ * in a database server-specific implementation as migration.[DB].class.php
+ */
+class Migration {
+
+    /**
+     * Origin DB type holder
+     */
+    public $odbtype = '';
+
+    /**
+     * Origin DB host holder
+     */
+    public $odbhost = '';
+
+    /**
+     * Origin DB port holder
+     */
+    public $odbport = '';
+
+    /**
+     * Origin DB user holder
+     */
+    public $odbuser = '';
+
+    /**
+     * Origin DB password holder
+     */
+    public $odbpass = '';
+
+    /**
+     * Origin DB name holder
+     */
+    public $odbname = '';
+
+    /**
+     * Array holding all errors/warnings ocurring during one execution
+     */
+    public $errors_stack = array();
+
+    /**
+     * Temporary handler for SQL result
+     */
+    public $odbrows = null;
+
+    /**
+     * Temporary holder for the list of users, courses and sessions and their 
+     * data. Store values here (preferably using the same indexes as the
+     * destination database field names) until ready to insert into Chamilo.
+     */
+    public $data_list = array('users' => array(), 'courses' => array(), 'sessions' => array());
+
+    /**
+     * The constructor assigns all database connection details to the migration
+     * object
+     * @param string The original database's host
+     * @param string The original database's port
+     * @param string The original database's user
+     * @param string The original database's password
+     * @param string The original database's name
+     * @return boolean False on error. Void on success.
+     */
+    public function __construct($dbhost, $dbport, $dbuser, $dbpass, $dbname) {
+        if (empty($dbhost) || empty($dbport) || empty($dbuser) || empty($dbpass) || empty($dbname)) {
+            $this->errors_stack[] = 'All origin database params must be given. Received ' . print_r(func_get_args(), 1);
+            return false;
+        }
+        $this->odbtype = $dbtype;
+        $this->odbhost = $dbhost;
+        $this->odbport = $dbport;
+        $this->odbuser = $dbuser;
+        $this->odbpass = $dbpass;
+        $this->odbname = $dbname;
+    }
+
+    /**
+     * The connect method should be extended by the child class
+     */
+    public function connect() {
+        //extend in child class
+    }
+
+    /**
+     * The migrate method launches the migration process based on an array of
+     * tables and fields matches defined in the given array.
+     * @param array Structured array of matches (see migrate.php)
+     */
+    public function migrate($matches) {
+        $found = false;
+        $table_idx = -1;
+        error_log("\n".'------------ Migration->migrate function called ------------'."\n");
+        
+        $extra_fields = array();
+                
+        // Browsing through 1st-level arrays in db_matches.php
+        foreach ($matches as $table) {
+            error_log('Found table ' . $table['orig_table']);
+            $build_only = false;
+            
+            if (empty($table['dest_table'])) {
+                //If there is no destination for this table, report
+                error_log(' ... which is just for data building');
+                $build_only = true;
+            }
+            
+            // Creating extra fields if necessary inside Chamilo (to store 
+            // original fields)
+            if (isset($table['extra_fields']) && in_array($table['dest_table'], array('course', 'user', 'session'))) {
+                $extra_fields = self::_create_extra_fields($table);
+            }
+            
+            // Process the migration of fields from the given table
+            $sql_select_fields = self::prepare_field_match($table);
+            $this->select_all($table['orig_table'], $sql_select_fields, $table);
+       
+            if (count($table['fields_match']) == 0) {
+                error_log('No fields found');
+                continue;
+            }            
+            $num_rows = $this->num_rows();
+            
+            if ($num_rows) {            
+                error_log('Records found: '.$num_rows);
+                $item = 1;
+                while ($row = $this->fetch_array()) {
+                    //error_log('Loading: ');error_log(print_r($row, 1));
+                    self::execute_field_match($table, $row, $extra_fields);
+                    $percentage = $item / $num_rows*100;
+                    if (round($percentage) % 10 == 0) {     
+                        $percentage = round($percentage, 3);
+                        error_log("Processing item {$table['orig_table']} #$item $percentage%") ;
+                    }
+                    $item++;
+                }                
+                error_log('Finished processing table ' . $table['orig_table']." \n\n");
+            } else {
+                error_log('No records found');
+            }
+            
+            //Stop here (only for tests)
+            if ($table['orig_table'] == 'ProgramaAcademico')  {
+                //exit;          
+            }
+        }
+    }
+    
+    function prepare_field_match($table) {        
+        $sql_select_fields = array();
+        foreach ($table['fields_match'] as $details) {
+            if (empty($details['orig'])) {
+                //Ignore if the field declared in $matches doesn't exist in
+                // the original database
+                continue;
+            }
+            $sql_select_fields[$details['orig']] = $details['orig'];
+            // If there is something to alter in the SQL query, rewrite the entry
+            if (!empty($details['sql_alter'])) {
+                $func_alter = $details['sql_alter'];
+                $sql_select_fields[$details['orig']] = MigrationCustom::$func_alter($details['orig']);
+            }
+            //error_log('Found field ' . $details['orig'] . ' to be selected as ' . $sql_select_fields[$details['orig']]);
+        }
+        return $sql_select_fields;     
+    }
+    
+    function execute_field_match($table, $row, $extra_fields) {        
+        //error_log('execute_field_match');
+        $dest_row = array();
+        $first_field = '';
+        $my_extra_fields = isset($extra_fields[$table['dest_table']]) ? $extra_fields[$table['dest_table']] : null;
+        $extra_field_obj = null;
+        $extra_field_value_obj = null;
+        
+        if (!empty($table['dest_table'])) {
+            $extra_field_obj = new Extrafield($table['dest_table']);        
+            $extra_field_value_obj = new ExtraFieldValue($table['dest_table']);
+        }        
+        $extra_fields_to_insert = array();
+        
+        foreach ($table['fields_match'] as $id_field => $details) {
+            if ($id_field == 0) {
+                $first_field = $details['dest'];
+            }
+            
+            if (isset($details['orig'])) {
+                $field_exploded = explode('.', $details['orig']);
+                if (isset($field_exploded[1])) {
+                    $details['orig'] = $field_exploded[1];
+                }
+            }
+            
+            // process the fields one by one
+            if ($details['func'] == 'none' || empty($details['func'])) {
+                $dest_data = $row[$details['orig']];
+            } else {
+                $dest_data = MigrationCustom::$details['func']($row[$details['orig']], $this->data_list, $row);
+            }
+            
+            if (isset($dest_row[$details['dest']])) {
+                $dest_row[$details['dest']] .= ' '.$dest_data;
+            } else {
+                $dest_row[$details['dest']] = $dest_data;
+            }
+                        
+            //Extra field values
+            $extra_field = isset($my_extra_fields) && isset($my_extra_fields[$details['dest']]) ? $my_extra_fields[$details['dest']] : null;
+            //error_log('-----');
+            //error_log(print_r($extra_field, 1));
+            if (!empty($extra_field) && $extra_field_obj) {
+                if (isset($extra_field['options'])) {
+                    $options = $extra_field['options'];
+                    $field_type = $extra_field['field_type'];
+                                        
+                    if (!empty($options)) {
+                        foreach ($options as $option) {
+                            foreach ($option as $key => $value) {
+                                //error_log("$key $value --> {$dest_row[$details['dest']]} ");
+                                if ($key == 'option_value' && $value == $dest_row[$details['dest']]) {
+                                    $value = $option['option_display_text'];                                    
+                                    if ($field_type == Extrafield::FIELD_TYPE_SELECT) {
+                                        $value = $option['option_value']; 
+                                    }                                    
+                                    $params = array(                
+                                        'field_id'      => $option['field_id'],
+                                        'field_value'   => $value,
+                                    );
+                                   break(2);
+                                }       
+                            }
+                        }
+                    }              
+                } else {
+                    $params = array(
+                        'field_id'      => $extra_field,
+                        'field_value'   => $dest_row[$details['dest']],
+                    );
+                }
+                if (!empty($params)) {
+                    $extra_fields_to_insert[] = $params;                
+                }
+                unset($dest_row[$details['dest']]);
+            }
+        }
+                
+        if (!empty($table['dest_func'])) {
+            //error_log('Calling '.$table['dest_func'].' on data recovered: '.print_r($dest_row, 1));            
+            $dest_row['return_item_if_already_exists'] = true;
+            
+            $item_result = call_user_func_array($table['dest_func'], array($dest_row, $this->data_list));
+            
+            if ($table['show_in_error_log'] == false) {
+                
+            } else {
+                error_log('Result of calling ' . $table['dest_func'] . ': ' . print_r($item_result, 1));
+            }
+            //error_log('Result of calling ' . $table['dest_func'] . ': ' . print_r($item_result, 1));
+            //After the function was executed fill the $this->data_list array
+            switch ($table['dest_table']) {
+                case 'course':
+                    //Saving courses in array
+                    if ($item_result) {                        
+                        //$this->data_list['courses'][$dest_row['uidIdCurso']] = $item_result;        
+                    } else {
+                        error_log('Course Not FOUND');                        
+                        error_log(print_r($item_result, 1));
+                        exit;
+                    }
+                    $handler_id = $item_result['code'];                  
+                    break;
+                case 'user':
+                    if (!empty($item_result)) {                        
+                        $handler_id = $item_result['user_id'];
+                        //error_log($dest_row['email'].' '.$dest_row['uidIdPersona']);
+                        if (isset($dest_row['uidIdAlumno'])) {
+                            //$this->data_list['users_alumno'][$dest_row['uidIdAlumno']]['extra'] = $item_result;
+                        }
+                        if (isset($dest_row['uidIdEmpleado'])) {
+                            //print_r($dest_row['uidIdEmpleado']);exit;                           
+                            //$this->data_list['users_empleado'][$dest_row['uidIdEmpleado']]['extra'] = $item_result;
+                        }                                          
+                    } else {
+                        global $api_failureList;
+                        error_log(print_r($api_failureList, 1));                        
+                    }       
+                    break;
+                case 'session':                     
+                    $this->data_list['sessions'][$dest_row['uidIdPrograma']] = $item_result;                    
+                    $handler_id = $item_result; //session_id
+                    break;
+            }
+            
+            //Saving extra fields of the element
+            if (!empty($extra_fields_to_insert)) {
+                foreach ($extra_fields_to_insert as $params) {                    
+                    $params[$extra_field_value_obj->handler_id] =  $handler_id;                                             
+                    $extra_field_value_obj->save($params);    
+                }
+            }
+        } else {
+           // $this->errors_stack[] = "No destination data dest_func found. Abandoning data with first field $first_field = " . $dest_row[$first_field];
+        }
+        unset($extra_fields_to_insert); //remove to free up memory
+        return $dest_row;
+    }
+    
+    /**
+     * Helper function to create extra fields in the Chamilo database
+     * @param Array An array containing an 'extra_fields' entry with details about the required extra fields
+     * @return void
+     */
+    private function _create_extra_fields(&$table) {
+        error_log('Inserting (if exists) extra fields for : ' . $table['dest_table']." \n");
+        foreach ($table['extra_fields'] as $extra_field) {
+            error_log('Preparing for insertion of extra field '.$extra_field['field_display_text']."\n");
+            $options = $extra_field['options'];
+            unset($extra_field['options']);
+            
+            $extra_field_obj = new ExtraField($table['dest_table']);
+            $extra_field_id = $extra_field_obj->save($extra_field);
+                    
+            $selected_fields = self::prepare_field_match($options);
+                        
+            //Adding options
+            if (!empty($options)) {       
+                $extra_field_option_obj = new ExtraFieldOption($table['dest_table']);
+                $this->select_all($options['orig_table'], $selected_fields);                        
+                $num_rows = $this->num_rows();
+
+                if ($num_rows) {
+                    $data_to_insert = array();
+                    $data_to_insert['field_id'] = $extra_field_id;
+
+                    while ($row = $this->fetch_array()) { 
+                        $data = self::execute_field_match($options, $row);                                
+                        $data_to_insert = array_merge($data_to_insert, $data);                                                              
+                        $extra_field_option_obj->save_one_item($data_to_insert, false, false);
+                        //error_log(print_r($extra_fields[$table['dest_table']]['extra_field_'.$extra_field['field_variable']], 1));
+                        $extra_fields[$table['dest_table']]['extra_field_'.$extra_field['field_variable']]['options'][] = $data_to_insert;                                
+                        $extra_fields[$table['dest_table']]['extra_field_'.$extra_field['field_variable']]['field_type'] = $extra_field['field_type'];
+                    }
+                    //$extra_fields[$table['dest_table']]['extra_field_'.$extra_field['field_variable']]['selected_option'] = 
+                    //error_log('$data: ' . print_r($data_to_insert, 1));
+                }
+            } else {
+                $extra_fields[$table['dest_table']]['extra_field_'.$extra_field['field_variable']] = $extra_field_id;
+            }
+        }
+        return $extra_fields;
+    }
+}
